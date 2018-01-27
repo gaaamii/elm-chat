2bd11c2edf4a8af7d5ffdb7ad6a394b536d1d102
gaaamii <i1shi4ga4mi1@gmail.com>
2018-01-27 15:41:41 +0900

    疲れてきたので一回コミット

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,8 @@ module Main exposing (..)
 import Html exposing (Html, button, div, img, program, text, textarea)
 import Html.Attributes exposing (src, value)
 import Html.Events exposing (onClick, onInput)
-import Styles
+import List exposing (..)
+import Styles as Styles
 
 
 main : Program Never Model Msg
@@ -16,90 +17,65 @@ main =
         }
 
 
-type alias Model =
-    { myselfId : String
-    , members : List Member
-    , talks : List Talk
-    , field : String
-    , nextTalkIdNum : Int
-    }
-
-
-emptyMember : Member
-emptyMember =
-    { id = ""
-    , name = ""
-    , imageUrl = ""
-    }
-
-
 type alias Talk =
-    { id : String
-    , memberId : String
+    { posterName : String
     , message : String
     , createdAt : String
-    }
-
-
-type alias Member =
-    { id : String
-    , name : String
     , imageUrl : String
     }
 
 
+type alias Model =
+    { talkList : List Talk
+    , content : String
+    }
+
+
 type Msg
-    = ChangeInput String
-    | Add
+    = InputForm String
+    | PostTalk
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( initialModel, Cmd.none )
+    ( { talkList = [ { posterName = "とみざわ", message = "ピザ食いてえ", createdAt = "2018/01/27 13:00", imageUrl = "http://www.hochi.co.jp/photo/20170718/20170718-OHT1I50084-T.jpg" }, { posterName = "伊達ちゃん", message = "ピザ食いてえ", createdAt = "2018/01/27 13:00", imageUrl = "http://www.hochi.co.jp/photo/20170718/20170718-OHT1I50084-T.jpg" } ]
+      , content = ""
+      }
+    , Cmd.none
+    )
 
-
-initialModel : Model
-initialModel =
-    { myselfId = "m2"
-    , members = initMembers
-    , talks = initTalks
-    , field = ""
-    , nextTalkIdNum = 3
-    }
-
-
-initMembers : List Member
-initMembers =
-    [ Member "m1" "伊達ちゃん" "https://imgcp.aacdn.jp/img-c/680/auto/tipsplus/series/246/20160608_1465380998273.jpg"
-    , Member "m2" "とみざわ" "http://www.hochi.co.jp/photo/20170718/20170718-OHT1I50084-T.jpg"
-    ]
-
-
-initTalks : List Talk
-initTalks =
-    [ Talk "t1" "m1" "ピザ食いてえ" "2018/01/27 13:00"
-    , Talk "t2" "m2" "ちょっと何いってるかわかんないっす" "2018/01/27 13:30"
-    ]
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
+update msg ({ talkList, content } as model) =
     case msg of
-        ChangeInput text ->
-            ( { model | field = text }, Cmd.none )
+        InputForm content ->
+            ( { model | content = content }, Cmd.none )
 
-        Add ->
+        PostTalk ->
             let
-                nextTalkId =
-                    "t" ++ toString model.nextTalkIdNum
-
-                newTalk =
-                    Talk nextTalkId model.myselfId model.field "2018/01/27 15:30"
+                newList =
+                    { posterName = "伊達ちゃん"
+                    , message = content
+                    , imageUrl = "https://imgcp.aacdn.jp/img-c/680/auto/tipsplus/series/246/20160608_1465380998273.jpg"
+                    , createdAt = "2018/01/27 13:00" -- 仮
+                    }
+                        :: talkList
             in
-            { model
-                | talks = model.talks ++ [ newTalk ]
-                , field = ""
-                , nextTalkIdNum = model.nextTalkIdNum + 1
-            } ! []
+            ( { model | talkList = newList, content = "" }, Cmd.none )
+
+
+viewTalk : Talk -> Html Msg
+viewTalk talk =
+    div [ Styles.talk ]
+        [ div [ Styles.talkLeft ]
+            [ img [ Styles.posterImg, src talk.imageUrl ] [] ]
+        , div [ Styles.talkRight ]
+            [ div [ Styles.posterName ] [ text talk.posterName ]
+            , div [ Styles.message ] [ text talk.message ]
+            , div [ Styles.talkFooter ]
+                [ text talk.createdAt ]
+            ]
+        ]
 
 
 view : Model -> Html Msg
@@ -110,35 +86,27 @@ view model =
                 [ img [ Styles.selfImg, src "http://www.hochi.co.jp/photo/20170718/20170718-OHT1I50084-T.jpg" ] []
                 ]
             , div [ Styles.formRight ]
-                [ textarea [ Styles.formArea, value model.field, onInput ChangeInput ] []
-                , button [ Styles.postButton, onClick Add ] [ text "投稿！" ]
+                [ textarea [ Styles.formArea, onInput (\str -> InputForm str), value model.content ] []
+                , button [ Styles.postButton, onClick PostTalk ] [ text "投稿！" ]
                 ]
             ]
-        , div [ Styles.talks ]
-            <| List.map (\talk -> viewTalk talk model) model.talks
-        ]
-
-
-viewTalk : Talk -> Model -> Html Msg
-viewTalk talk model =
-    let
-        member =
-            model.members
-                |> List.filter (\member -> member.id == talk.memberId)
-                >> List.head
-                >> Maybe.withDefault emptyMember
-    in
-    div [ Styles.talk ]
-        [ div [ Styles.talkLeft ]
-            [ img [ Styles.posterImg, src member.imageUrl ] [] ]
-        , div [ Styles.talkRight ]
-            [ div [ Styles.posterName ] [ text member.name ]
-            , div [ Styles.message ] [ text talk.message ]
-            , div [ Styles.talkFooter ]
-                [ text talk.createdAt]
+        , div [] <| List.map viewTalk model.talkList
+        , div [ Styles.talk ]
+            [ div [ Styles.talkLeft ]
+                [ img [ Styles.posterImg, src "http://www.hochi.co.jp/photo/20170718/20170718-OHT1I50084-T.jpg" ] [] ]
+            , div [ Styles.talkRight ]
+                [ div [ Styles.posterName ] [ text "とみざわ" ]
+                , div [ Styles.message ] [ text "ちょっと何言ってるかわかんないっす" ]
+                , div [ Styles.talkFooter ]
+                    [ text "2018/01/27 13:30"
+                    , div [ Styles.buttons ]
+                        [ button [ Styles.editButton ] [ text "編集" ]
+                        , button [ Styles.deleteButton ] [ text "削除" ]
+                        ]
+                    ]
+                ]
             ]
         ]
-
 
 
 
